a3df3044e0b4c1f9b21a433feb5f2ffc7b4eaf50
JeUnsong <zzangjh5@gmail.com>
2022-05-16 18:19:01 +0900

    index 컨트롤러 추가

---
+++ src/main/java/com/example/sh/controller/SHController.java
@@ -0,0 +1,17 @@
+package com.example.sh.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/house")
+@Log4j2
+public class SHController {
+
+    @GetMapping("/index")
+    public void index(){
+        log.info("hi log");
+    }
+}
