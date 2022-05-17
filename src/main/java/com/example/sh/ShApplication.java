8419c1d8be203deb303be72c436642dd1d145881
JeUnsong <zzangjh5@gmail.com>
2022-05-17 18:11:36 +0900

    변동 없음

--- src/main/java/com/example/sh/ShApplication.java
+++ src/main/java/com/example/sh/ShApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ShApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(ShApplication.class, args);
     }
 
 }
