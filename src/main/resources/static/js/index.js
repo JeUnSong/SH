06e4c8d0e6ab926715475be20f272bc3dd7e59a6
JeUnSong <94476600+JeUnSong@users.noreply.github.com>
2022-05-19 21:09:32 +0900

    스크롤 상세 조정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,9 +3,33 @@ $( document ).ready( function() {
 
     $( window ).scroll( function() {
         if ( $( document ).scrollTop() > jbOffset.top ) {
-                $( '.search-container' ).addClass( 'search-fixed' );
+                $( '.search-container').addClass( 'search-fixed' );
         }else {
             $( '.search-container' ).removeClass( 'search-fixed' );
+        }
+    });
+});
+
+$( document ).ready( function() {
+    var jbOffset = $( '.search-container' ).offset();
+
+    $( window ).scroll( function() {
+        if ( $( document ).scrollTop() > jbOffset.top ) {
+            $( '.search-item').addClass( 'search-scale' );
+        }else {
+            $( '.search-item' ).removeClass( 'search-scale' );
+        }
+    });
+});
+
+$( document ).ready( function() {
+    var jbOffset = $( '.search-container' ).offset();
+
+    $( window ).scroll( function() {
+        if ( $( document ).scrollTop() > jbOffset.top ) {
+            $( '.search-icon').addClass( 'search-icon-scroll' );
+        }else {
+            $( '.search-icon' ).removeClass( 'search-icon-scroll' );
         }
     });
 });
